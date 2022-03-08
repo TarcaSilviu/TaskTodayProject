51eaa79e7f93a86c1ceac8fc0d0012979017132d
TarcaSilviu <silviutarca@gmai.com>
2022-03-08 12:27:45 +0200

    Update

--- app/src/main/java/com/example/tasktodayappproject/MenuActivity.java
+++ app/src/main/java/com/example/tasktodayappproject/MenuActivity.java
@@ -8,27 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class UserHub extends AppCompatActivity {
-
-        ImageButton backImageButton;
+public class MenuActivity extends AppCompatActivity {
+    ImageButton backImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_hub);
-        configureMenuButton();
-        backImageButton=(ImageButton) findViewById(R.id.BackIcon);
+        confirgureBackButton();
+        backImageButton=(ImageButton) findViewById(R.id.MenuIcon);
         backImageButton.setVisibility(View.GONE);
     }
-    private void configureMenuButton(){
-        ImageButton menuButton=(ImageButton) findViewById(R.id.MenuIcon);
+    private void confirgureBackButton(){
+        ImageButton backImageButton=(ImageButton) findViewById(R.id.BackIcon);
 
-        menuButton.setOnClickListener(new View.OnClickListener() {
+        backImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(UserHub.this,MenuActivity.class));
+                startActivity(new Intent(MenuActivity.this,UserHub.class));
             }
         });
     }
-
-
 }

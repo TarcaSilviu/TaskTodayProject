45ec241a395ee51d99f5345d60cc80f24d5e41be
TarcaSilviu <silviutarca@gmai.com>
2022-03-08 11:55:16 +0200

    Update

--- app/src/main/java/com/example/tasktodayappproject/UserHub.java
+++ app/src/main/java/com/example/tasktodayappproject/UserHub.java
@@ -3,12 +3,19 @@ package com.example.tasktodayappproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class UserHub extends AppCompatActivity {
 
+        ImageButton backImageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_hub);
+        backImageButton=(ImageButton) findViewById(R.id.BackIcon);
+        backImageButton.setVisibility(View.GONE);
     }
+
+
 }
